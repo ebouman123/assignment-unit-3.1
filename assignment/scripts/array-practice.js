2463cc6e521504c95790ad3eab97fba6e3ed5b15
Evan Bouman <evan.bouman@gmail.com>
2024-08-15 09:56:36 -0500

    Created favoriteFoods array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -11,6 +11,8 @@ console.log('Animals are: ', animalArray);
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
 
+let favoriteFoods = [`spaghetti`, `bulgogi`, `ramen`];
+console.log(`Some of my favorite foods are: `, favoriteFoods);
 
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
